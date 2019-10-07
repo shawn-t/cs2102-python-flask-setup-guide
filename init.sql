04b9cbe39c0caee79f5753978a0056b72d76f335
Lee Jin Yao <35756209+jylee-git@users.noreply.github.com>
2019-10-07 22:04:10 +0800

    Transfer create table sql code from docs to init.sql (#2)
    * Transferred the code from docs into init.sql
    
    * Removed the skeleton code

--- init.sql
+++ init.sql
@@ -1,6 +1,55 @@
-CREATE TABLE web_user(
-    username VARCHAR PRIMARY KEY NOT NULL,
-    first_name VARCHAR,
-    last_name VARCHAR,
-    password VARCHAR NOT NULL
+CREATE TABLE User (
+	username varchar(50) PRIMARY KEY,
+    firstName varchar(20),
+    lastName varchar(20),
+    address varchar(50),
+    password varchar(50) NOT NULL,
+    phoneNumber integer
 );
+
+CREATE TABLE Driver (
+	username varchar(50) PRIMARY KEY REFERENCES User (username),
+	d_rating integer,
+	license_no integer
+);
+
+CREATE TABLE Passenger (
+	username varchar(50) PRIMARY KEY REFERENCES User (username),
+	p_rating integer
+);
+
+CREATE TABLE Model (
+	name varchar(20) PRIMARY KEY,
+    size integer
+);
+
+CREATE TABLE Car (
+	plate number varchar(20) PRIMARY KEY,
+    colours  varchar(20)
+);
+
+CREATE TABLE Promo (
+	promoCode varchar(20) PRIMARY KEY,
+	quotaLeft integer,
+	maxDiscount integer,
+	minPrice integer,
+	discount integer	
+);
+
+CREATE TABLE Ride (
+	rideID varchar(20) PRIMARY KEY,
+    p_comment varchar(50),
+    p_rating integer,
+    d_comment varchar(50),
+    d_rating integer	
+);
+
+CREATE TABLE Advertisement (
+	timePosted integer PRIMARY KEY,
+    numPassengers integer,
+    departureTime integer,
+    price integer,
+    to varchar(50),
+    from varchar(50)
+);
+
